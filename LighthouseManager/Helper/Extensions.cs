858c62992588c677b79539ec7ad228e1461aeff4
Daniel Däschner <dad@surdata.de>
2021-01-11 21:24:02 +0100

    Filenames

--- LighthouseManager/Helper/Extensions.cs
+++ LighthouseManager/Helper/Extensions.cs
@@ -1,7 +1,21 @@
-﻿namespace LighthouseManager.Helper
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace LighthouseManager.Helper
 {
-    public enum Powerstate
+    public static class Extensions
     {
-        On, Off, Sleep, Standby
+        public static ulong ToMacUlong(this string macAddress)
+        {
+            var hex = macAddress.Replace(":", "");
+            return Convert.ToUInt64(hex, 16);
+        }
+
+        public static string ToMacString(this ulong macAddress)
+        {
+            var regex = "(.{2})(.{2})(.{2})(.{2})(.{2})(.{2})";
+            var replace = "$1:$2:$3:$4:$5:$6";
+            return Regex.Replace(macAddress.ToString("X"), regex, replace);
+        }
     }
 }
